a2821fcf51b2788ddefa0c766d7714c6b309adbf
2nofa11 <aboubthethirdkimi@icloud.com>
2022-09-29 20:05:10 +0900

    p27 変数名を式として使う

--- src/02_Basic/文と式.ts
+++ src/02_Basic/文と式.ts
@@ -1,7 +1,13 @@
 //2.1.1  文と式
 
-export const sentence = () => {
+export const sentence1 = () => {
   const greeting = "Hello, ";
   const target = "World!";
+  return greeting + target;
+};
+
+export const sentence2 = () => {
+  const greeting = "Hello, ";
+  const target = greeting;
   return greeting + target;
 };
