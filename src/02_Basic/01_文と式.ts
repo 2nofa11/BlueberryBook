ee0126279b5e76e9fa5217a6bb2814ae0125df67
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-01 00:09:15 +0900

    p29 複数の変数を,でつなげる（近年は不人気）

---
+++ src/02_Basic/01_文と式.ts
@@ -0,0 +1,19 @@
+//2.1.1  文と式
+
+export const sentence1 = () => {
+  const greeting = "Hello, ";
+  const target = "World!";
+  return greeting + target;
+};
+
+export const sentence2 = () => {
+  const greeting = "Hello, ";
+  const target = greeting;
+  return greeting + target;
+};
+
+export const sentence3 = () => {
+  const greeting = "Hello, ";
+  const target = greeting + "World!";
+  return target;
+};
