7822ee852beb9694e5f3376947507737d2e786e3
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-01 12:39:02 +0900

    p35 数値はnumber型、+*/で計算、少数点の区別はない

---
+++ src/02_Basic/03_プリミティブ型.ts
@@ -0,0 +1,18 @@
+// プリミティブ型とは？
+
+// 文字列
+// 数値
+// 審議値
+// Bigint
+// null
+// undefined
+// 複雑なデータ型もプリミティブ型の組み合わせ
+
+// 雰囲気はこんな感じ
+export const sentence1 = () => {
+  const width1 = 5;
+  const width2 = 8;
+  const height: number = 3;
+  const area = ((width1 + width2) * height) / 2;
+  return area;
+};
