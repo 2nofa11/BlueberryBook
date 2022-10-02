0312650b77cda0e65c1a405f49ecadab61727905
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-02 10:04:46 +0900

    理論演算子「? :」を使うん ?の左はT/F以外でも利用できる

--- src/02_Basic/04_演算子.ts
+++ src/02_Basic/04_演算子.ts
@@ -76,3 +76,16 @@ export const shortCircuitRating2 = () => {
     rl.close();
   });
 };
+
+// 条件演算子
+export const ConditionalOperator = () => {
+  rl.question("数値を入力してください", (line) => {
+    const num = Number(line);
+    const message =
+      0 <= num && num < 100
+        ? `${num}は0以上100未満です。`
+        : `${num}は0以上100未満ではありません`;
+    console.log(message);
+    rl.close();
+  });
+};
