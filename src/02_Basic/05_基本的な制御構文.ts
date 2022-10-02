1d612aacce1eb404b4f74119ed75953ecb193a8a
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-02 10:27:03 +0900

    p63 ブロック文「{}」の説明

--- src/02_Basic/05_基本的な制御構文.ts
+++ src/02_Basic/05_基本的な制御構文.ts
@@ -2,3 +2,10 @@ export const if1 = (userName: string) => {
   if (userName === "") userName = "名無し";
   console.log(userName);
 };
+
+export const if2 = (userName: string) => {
+  if (userName === "") {
+    console.log("名前入れて");
+    userName = "名無し";
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import { if1 } from "./02_Basic/05_基本的な制御構文";
+import { if2 } from "./02_Basic/05_基本的な制御構文";
 
-if1("");
+if2("");
