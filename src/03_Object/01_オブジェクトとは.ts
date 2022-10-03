84548ca5c3deee73f0205032537b26c09c58ccd3
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-04 07:40:54 +0900

    p76 オブジェクトリテラルの一部では、条件演算子が使える。 プロパティ名を変数名が一致の場合は省略可能

--- src/03_Object/01_オブジェクトとは.ts
+++ src/03_Object/01_オブジェクトとは.ts
@@ -6,3 +6,14 @@ export const associativeArray = () => {
   console.log(obj.foo);
   console.log(obj.bar);
 };
+
+export const basic1 = (input: string) => {
+  const age = 28;
+  const user = {
+    // 演算子使用
+    user: input ? input : "名無し",
+    // プロパティ:変数名
+    age,
+  };
+  console.log(user);
+};
