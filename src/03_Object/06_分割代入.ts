75c8c8b1af72fb73a484a180a951794e6c6c298a
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-10 08:10:13 +0900

    p115 プロパティ名と別名を使用する場合

--- src/03_Object/06_分割代入.ts
+++ src/03_Object/06_分割代入.ts
@@ -1,5 +1,11 @@
-export const destructuringAssignment = () => {
+export const destructuringAssignment1 = () => {
   const obj = { foo: "フー", bar: "バー" };
   const { foo, bar } = obj;
   console.log(foo, bar);
+};
+
+export const destructuringAssignment2 = () => {
+  const obj = { foo: "フー", bar: "バー", "foo bar": "フーバー" };
+  const { foo, bar: barVar, "foo bar": fooBar } = obj;
+  console.log(foo, barVar, fooBar);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import { destructuringAssignment } from "./03_Object/06_分割代入";
-destructuringAssignment();
+import { destructuringAssignment2 } from "./03_Object/06_分割代入";
+destructuringAssignment2();
