c9eb1468f86c31b644f3db5f5fc67648a116189e
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-09 14:49:45 +0900

    p102 extendsを使った、部分型関係における型引数の制約

--- src/03_Object/04_型引数を持つ型.ts
+++ src/03_Object/04_型引数を持つ型.ts
@@ -9,3 +9,29 @@ export const typeArgument = () => {
   };
   console.log(user);
 };
+
+export const useExtends = () => {
+  type HasName = {
+    name: string;
+  };
+  type Family<Parent extends HasName, Child extends Parent> = {
+    mother: Parent;
+    father: Parent;
+    child: Child;
+  };
+
+  type Animal = {
+    name: string;
+  };
+  type Human = {
+    name: string;
+    age: number;
+  };
+  type T = Family<Animal, Human>;
+  const t: T = {
+    mother: { name: "mom" },
+    father: { name: "dad" },
+    child: { name: "kid", age: 10 },
+  };
+  console.log(t);
+};
