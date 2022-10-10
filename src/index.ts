d69cee548db30c9b1280935f00d2d17b11b165ed
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-10 13:47:04 +0900

    p125 正規表現の利用 ①replaceを用いるときは、gフラグで全件あることを明示する必要あり。 ②matchでは、キャプチャリンググループで配列を取得する方法もあり

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import { regularExpression1 } from "./03_Object/07_その他の組み込みオブジェクト";
-regularExpression1();
+import { regularExpression2 } from "./03_Object/07_その他の組み込みオブジェクト";
+regularExpression2();
