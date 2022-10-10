b0324a61cdeced9b40328a466673f99d46ab942b
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-11 07:25:49 +0900

    p138 戻り値がない関数。早期returnを使ってネストを深くすることを避けている

--- src/04_Function/01_関数の作り方.ts
+++ src/04_Function/01_関数の作り方.ts
@@ -5,3 +5,13 @@ export function range(min: number, max: number): number[] {
   }
   return result;
 }
+
+export function helloWorldNTTimes(n: number): void {
+  if (n >= 100) {
+    console.log(`${n}回なんてむり！`);
+    return;
+  }
+  for (let i = 0; i <= n; i++) {
+    console.log("Hello,world!");
+  }
+}
