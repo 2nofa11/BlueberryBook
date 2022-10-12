74a4b0299e3fa8387937b2ac2cb3c0c4b9d2106d
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-12 22:14:25 +0900

    p144 可変長引数の宣言 ...を使って、引数を配列として取得できる。

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-import { method } from "./04_Function/01_関数の作り方";
-console.log(method.double1(100));
-console.log(method.double2(100));
+import { restParameters } from "./04_Function/01_関数の作り方";
+console.log(restParameters(1, 2, 3));
