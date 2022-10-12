7dbad59128a2e98de92c2814c9156473c3e56a92
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-12 22:39:48 +0900

    p148 オプショナルな引数の宣言方法

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-import { spread } from "./04_Function/01_関数の作り方";
-spread();
+import { toLowerOrUpper } from "./04_Function/01_関数の作り方";
+toLowerOrUpper("hello");
+toLowerOrUpper("hello", false);
+toLowerOrUpper("hello", true);
