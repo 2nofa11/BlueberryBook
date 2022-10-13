e93a1558f6ffa58c1a310d634748b668f19a4a3b
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-14 07:39:41 +0900

    p154 関数が戻り値を返さない場合も型注釈を省略できる

--- src/04_Function/02_関数の型.ts
+++ src/04_Function/02_関数の型.ts
@@ -2,3 +2,9 @@ export const xRepeat1: F = (num: number): string => "x".repeat(num);
 type F = (num: number) => string;
 
 export const xRepeat2 = (num: number) => "x".repeat(num);
+
+export const g = (num: number) => {
+  for (let i = 0; i < num; i++) {
+    console.log("X");
+  }
+};
