341246621414eb7596d16665420cbd734f3ef672
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-14 07:37:02 +0900

    p154 戻り値の型注釈を省略

--- src/04_Function/02_関数の型.ts
+++ src/04_Function/02_関数の型.ts
@@ -1,2 +1,4 @@
-export const xRepeat: F = (num: number): string => "x".repeat(num);
+export const xRepeat1: F = (num: number): string => "x".repeat(num);
 type F = (num: number) => string;
+
+export const xRepeat2 = (num: number) => "x".repeat(num);
