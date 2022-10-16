469716b54d4a6e86196b8d89f2bbb296be570bc8
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-16 22:13:31 +0900

    p169 ジェネリクス：型引数を受け取る関数を作る機能 「入力がどんな値でもいいな」と思ったときは使用のチャンス

---
+++ src/04_Function/04_ジェネリクス.ts
@@ -0,0 +1,11 @@
+export const generics = () => {
+  function repeat<T>(element: T, length: number): T[] {
+    const result: T[] = [];
+    for (let i = 0; i < length; i++) {
+      result.push(element);
+    }
+    return result;
+  }
+  console.log(repeat<string>("a", 5));
+  console.log(repeat<number>(0, 5));
+};
