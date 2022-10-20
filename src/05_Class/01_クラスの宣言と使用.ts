ddc95e79fba0b4a8b5c2981925b9f3ac50fe600b
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-20 22:20:25 +0900

    p188 オプショナルに宣言するとundefinedで省略できる

--- src/05_Class/01_クラスの宣言と使用.ts
+++ src/05_Class/01_クラスの宣言と使用.ts
@@ -1,11 +1,12 @@
 class User {
-    name:string ="";
+    name?:string;
     age:number = 0;
 }
 
 export const usingClass = () =>{
     const uhyo = new User();
     console.log(uhyo);
+    uhyo.name = "uhyo";
     uhyo.age = 20;
     console.log(uhyo);
 }
