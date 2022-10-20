4eadfae8dea70e722b3c524d5d8df62778eb5afd
akito.tsunoda@rakus.co.jp <akito.tsunoda@rakus.co.jp>
2022-10-20 22:21:21 +0900

    p189 readonlyの利用

--- src/05_Class/01_クラスの宣言と使用.ts
+++ src/05_Class/01_クラスの宣言と使用.ts
@@ -1,12 +1,11 @@
 class User {
     name?:string;
-    age:number = 0;
+    readonly age:number = 0;
 }
 
 export const usingClass = () =>{
     const uhyo = new User();
     console.log(uhyo);
     uhyo.name = "uhyo";
-    uhyo.age = 20;
     console.log(uhyo);
 }
