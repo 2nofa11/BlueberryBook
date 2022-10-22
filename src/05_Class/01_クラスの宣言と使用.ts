86d514cab2421aebf2cbbc9c44545d903b5f4c02
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-22 16:11:04 +0900

    p200 静的初期化メソッドを使って特権的な処理を作成することができる

--- src/05_Class/01_クラスの宣言と使用.ts
+++ src/05_Class/01_クラスの宣言と使用.ts
@@ -4,8 +4,12 @@ class User {
     return new User(User.adminName, 23);
   }
 
+  static adminUser: User;
+  static {
+    this.adminUser = new User("admin", 9999);
+  }
   public name: string;
-  #age: number;
+  #age: number = 0;
 
   constructor(name: string, age: number) {
     this.name = name;
@@ -16,14 +20,18 @@ class User {
     return this.#age >= 20;
   }
 
-  public setAge(newAge: number) {
-    this.#age = newAge;
+  public setAge(age: number) {
+    if (age < 0 || age > 150) return;
+    this.#age = age;
+  }
+  public getAge() {
+    return this.#age;
   }
 }
 
 export const usingClass = () => {
   const uhyo = new User("uhyo", 1);
-  //   console.log(uhyo.#age);
+  console.log(User.adminUser.getAge());
 };
 
 export const staticInitializationBlock = () => {
