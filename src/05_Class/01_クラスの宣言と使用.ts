9a40640c16a2e7ddfbccc745fba12a9f34769ca7
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-22 15:46:18 +0900

    p198 #を使ったプライベートプロパティの定義 #はES2015にて制定

--- src/05_Class/01_クラスの宣言と使用.ts
+++ src/05_Class/01_クラスの宣言と使用.ts
@@ -1,21 +1,27 @@
-const User = class {
+class User {
   static adminName: string = "uhyo";
   static getAdminUser() {
     return new User(User.adminName, 23);
   }
 
-  constructor(public name: string, private age: number = 22) {}
+  public name: string;
+  #age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.#age = age;
+  }
 
   isAdult(): boolean {
-    return this.age >= 20;
+    return this.#age >= 20;
   }
 
   public setAge(newAge: number) {
-    this.age = newAge;
+    this.#age = newAge;
   }
-};
+}
 
 export const usingClass = () => {
   const uhyo = new User("uhyo", 1);
-  console.log(uhyo.isAdult());
+  console.log(uhyo.#age);
 };
