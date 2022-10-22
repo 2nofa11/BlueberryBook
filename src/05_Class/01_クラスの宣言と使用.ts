9550929cade9e12a48a928eb7bbbfe640e164b98
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-22 15:25:38 +0900

    p195 privateを使ってインスタンスのプロパティやメソッドを外向きのインターフェースと内部実装に区別する

--- src/05_Class/01_クラスの宣言と使用.ts
+++ src/05_Class/01_クラスの宣言と使用.ts
@@ -5,7 +5,7 @@ class User {
   }
 
   name?: string;
-  age: number = 0;
+  private age: number = 0;
 
   constructor(name: string, age: number) {
     this.name = name;
@@ -16,17 +16,12 @@ class User {
     return this.age >= 20;
   }
 
-  setAge(newAge: number) {
+  public setAge(newAge: number) {
     this.age = newAge;
   }
 }
 
 export const usingClass = () => {
-  console.log(User.adminName);
-  const admin = User.getAdminUser();
-  console.log(admin.age);
-  console.log(admin.isAdult());
-
-  const uhyo = new User("a", 28);
-  //   console.log(uhyo.adminName)
+  const uhyo = new User("uhyo", 28);
+  //   console.log(uhyo.age); errorになる
 };
