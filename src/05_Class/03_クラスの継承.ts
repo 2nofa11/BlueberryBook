3d2e4635ac458aaa454fbf7bb94f590ee40f5a95
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-24 07:34:41 +0900

    p215 オーバーライドを使って、継承を明示する

--- src/05_Class/03_クラスの継承.ts
+++ src/05_Class/03_クラスの継承.ts
@@ -1,34 +1,34 @@
-import exp = require("constants")
+import exp = require("constants");
 
 class User {
-  name:string
-  #age:number
+  name: string;
+  #age: number;
 
-  constructor(name:string,age:number){
-    this.name = name
-    this.#age = age
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.#age = age;
   }
 
-  public isAdult():boolean{
-    return this.#age >= 20
+  public isAdult(): boolean {
+    return this.#age >= 20;
   }
 }
 
 class PremiumUser extends User {
-  rank: number 
-  constructor(name:string,age:number,rank:number){
-    super(name,age)
-    this.rank = rank
+  rank: number;
+  constructor(name: string, age: number, rank: number) {
+    super(name, age);
+    this.rank = rank;
   }
 
-  public isAdult(): boolean {
-    return true
+  public override isAdult(): boolean {
+    return true;
   }
 }
 
 export const usingPremiumUser = () => {
-  const uhyo = new PremiumUser("uhyo",10,2)
-  console.log(uhyo.rank)
-  console.log(uhyo.name)
-  console.log(uhyo.isAdult())
-}
+  const uhyo = new PremiumUser("uhyo", 10, 2);
+  console.log(uhyo.rank);
+  console.log(uhyo.name);
+  console.log(uhyo.isAdult());
+};
