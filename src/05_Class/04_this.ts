6ba97cc994df85d627840dcd38cf56e54c91ab7d
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-24 21:37:46 +0900

    p223 thisはオブジェクト以外でも利用可能

--- src/05_Class/04_this.ts
+++ src/05_Class/04_this.ts
@@ -20,3 +20,17 @@ export const usingThis = () => {
   console.log(jhon.isAdult());
   console.log(uhyo.isAdult() === jhon.isAdult());
 };
+
+const user = {
+  name: "uhyo",
+  age: 26,
+  isAdult() {
+    return this.age >= 20;
+  },
+};
+
+export const userObj = () => {
+  console.log(user.isAdult());
+  user.age = 10;
+  console.log(user.isAdult());
+};
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import { usingThis } from "./05_Class/04_this";
-usingThis();
+import { userObj } from "./05_Class/04_this";
+userObj();
