9cb5e00cfd5c68fd546a6a555f199dca3882a1f1
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-28 07:45:21 +0900

    p244 クラスを使わず実行

--- src/05_Class/06_力試し.ts
+++ src/05_Class/06_力試し.ts
@@ -21,3 +21,9 @@ export const useUser = () => {
   const uhyo = new User("uhyo", 26);
   console.log(uhyo.getMessage("こんにちは"));
 };
+
+export const createUser = (n: string, a: number) => {
+  return function (m: string): string {
+    return `${n}(${a})「${m}」`;
+  };
+};
