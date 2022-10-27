83a858a35c395cf429bd4151bbd6e2d8a10418f6
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-28 07:37:00 +0900

    p244 関数をクラスに変えてみる

--- src/05_Class/06_力試し.ts
+++ src/05_Class/06_力試し.ts
@@ -1,1 +1,23 @@
 // クラスが便利なのは、「メソッドを持ったオブジェクト」を作る場合
+
+class User {
+  readonly name: string;
+  readonly age: number;
+
+  constructor(n: string, a: number) {
+    if (n === "") {
+      throw Error("名前は空にできません");
+    }
+    this.name = n;
+    this.age = a;
+  }
+  // メソッドとしてオブジェクトに備える
+  getMessage(message: string): string {
+    return `${this.name}(${this.age})「${message}」`;
+  }
+}
+
+export const useUser = () => {
+  const uhyo = new User("uhyo", 26);
+  console.log(uhyo.getMessage("こんにちは"));
+};
