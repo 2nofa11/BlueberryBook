448c4eec6165ac4018327d2b018e8e89f9652c62
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-30 08:57:09 +0900

    p265 テンプレートリテラル型の使い方③

--- src/06_Type/02_リテラル型.ts
+++ src/06_Type/02_リテラル型.ts
@@ -11,3 +11,9 @@ export function getHelloStr(): `Hello, ${string}` {
     return "Hello, my world";
   }
 }
+
+function makeKey<T extends string>(userName: T) {
+  return `user:${userName}` as const;
+}
+
+const uhyoKey: "user:uhyo" = makeKey("uhyo");
