c548314fc106f934aecf6eba7b953a6ff230c8bd
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-30 08:52:24 +0900

    p264 テンプレートリテラル型の利用①

--- src/06_Type/02_リテラル型.ts
+++ src/06_Type/02_リテラル型.ts
@@ -2,3 +2,12 @@ const foo: "foo" = "foo";
 const one: 1 = 1;
 const t: true = true;
 const three: 3n = 3n;
+
+export function getHelloStr(): `Hello, ${string}` {
+  const rand = Math.random();
+  if (rand < 0.3) {
+    return "Hello, world";
+  } else {
+    return "Hello, my world";
+  }
+}
