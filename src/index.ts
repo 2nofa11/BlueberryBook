ba86037e51532cf2c74072c8975eabdaec2b0c53
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-30 10:00:28 +0900

    p273 typeof演算子を用いる絞り込み

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import { numberWithSign } from "./06_Type/03_型の絞り込み";
-console.log(numberWithSign(5, "none"));
+import { formatNUmberOrString } from "./06_Type/03_型の絞り込み";
+console.log(formatNUmberOrString("しゅごい"));
