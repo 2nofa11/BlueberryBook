455750351ab28721429bf1087c5977fe0aa91b10
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-02 07:49:23 +0900

    p288 asの正しい使い方 ・不正確な型を正しく直すために使っている ・できる限りasを使う場合はコメントにその理由をのこす

--- src/06_Type/05_asによる型アサーション.ts
+++ src/06_Type/05_asによる型アサーション.ts
@@ -2,3 +2,32 @@ export function getFirstFiveLetters(strOrNum: string | number) {
   const str = strOrNum as string;
   return str.slice(0, 5);
 }
+
+type Animal = {
+  tag: "animal";
+  species: string;
+};
+
+type Human = {
+  tag: "human";
+  name: string;
+};
+
+type User = Animal | Human;
+
+function getNameIfAllHuman(users: readonly User[]): string[] | undefined {
+  // every:配列がすべて条件を満たす場合にTrue
+  if (users.every((user) => user.tag === "human")) {
+    // 条件文により「Human[]」しか入らない
+    return (users as Human[]).map((users) => users.name);
+  }
+  return undefined;
+}
+
+export const exeNames = () => {
+  const humans: User[] = [
+    { name: "uhyo", tag: "human" },
+    { name: "jhon", tag: "human" },
+  ];
+  return getNameIfAllHuman(humans);
+};
