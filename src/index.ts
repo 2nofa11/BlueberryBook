8897351cee2f855e1d2ca25127075ccb3d700fde
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-02 10:16:57 +0900

    p298 object型を使って、型を制限する

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-import { doNothing } from "./06_Type/06_any型をunknown型";
-console.log(doNothing("100"));
+import { useToString } from "./06_Type/07_さらに高度な型";
+console.log(
+  useToString({
+    toString() {
+      return "aa";
+    },
+  })
+);
+// objectと定義することで入ってくる型を制限する
+// console.log(useToString(3.14));
