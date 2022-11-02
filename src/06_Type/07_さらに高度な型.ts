0c5e62e8745f02fcd6c7e8a13fd20f2931f51832
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-02 10:25:45 +0900

    p299 当てはまる型が存在しない場合はnever型にて定義する

--- src/06_Type/07_さらに高度な型.ts
+++ src/06_Type/07_さらに高度な型.ts
@@ -5,3 +5,9 @@ type HasToString = {
 export function useToString(value: HasToString & object) {
   console.log(`value is ${value.toString()}`);
 }
+
+function useNever(value: never) {
+  const num: number = value;
+  const str: string = value;
+  const obj: object = value;
+}
