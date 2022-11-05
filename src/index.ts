91b8db95ba1eba156a09347bdd732a42a87a1d1a
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-06 08:06:54 +0900

    p323 デフォルトエクスポートとデフォルトインポート

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import {
-  age as uhyoAge,
-  getUhyoName,
-} from "./07_モジュールシステム/01_import宣言とexport宣言.js";
-
-console.log(`名前は${getUhyoName()}、歳は${uhyoAge}です。`);
+import increment, { value } from "./07_モジュールシステム/counter";
+console.log(increment());
+console.log(increment());
+console.log(increment());
+console.log(value);
