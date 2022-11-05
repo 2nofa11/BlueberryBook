48086d6313b795af61007d128c5a759c3cb575c3
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-05 16:09:52 +0900

    p314 タグ付きユニオンの場合はswitchができしている。個々のコーティングむずすぎるよ、、、

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { answer2, Option } from "./06_Type/08_力試し";
+import { answer2, Option, doubleOption } from "./06_Type/08_力試し";
 
 const four: Option<number> = {
   tag: "some",
   value: 1000,
 };
 
-answer2(four);
-answer2({ tag: "none" });
+doubleOption(four);
+doubleOption({ tag: "none" });
