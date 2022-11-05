3ef006647669f55d60571e52a8d31a2b3c476f74
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-06 07:29:50 +0900

    p317 基本的な宣言方法

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import { answer2, Option, doubleOption } from "./06_Type/08_力試し";
+import {
+  age,
+  name,
+} from "./07_モジュールシステム/01_import宣言とexport宣言.js";
 
-const four: Option<number> = {
-  tag: "some",
-  value: 1000,
-};
-
-doubleOption(four);
-doubleOption({ tag: "none" });
+console.log(`名前は${name}、歳は${age}です。`);
