bd7c4fd43310290495e615d4dc5df6fdf25f6d6b
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-05 12:38:02 +0900

    p312 タグ付きユニオンの練習

--- src/06_Type/08_力試し.ts
+++ src/06_Type/08_力試し.ts
@@ -1,8 +1,14 @@
-type Option<T> = { tag: true; value: T } | { tag: false };
+export type Option<T> = { tag: "some"; value: T } | { tag: "none" };
 
 export function answer1() {
-  const a: Option<number> = { tag: true, value: 100 };
+  const a: Option<number> = { tag: "some", value: 100 };
   console.log(a);
-  const b: Option<undefined> = { tag: false };
+  const b: Option<undefined> = { tag: "none" };
   console.log(b);
+}
+
+export function answer2(obj: Option<number>) {
+  if (obj.tag === "some") {
+    console.log(obj.value);
+  }
 }
