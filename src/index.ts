da1e476743ae50f862ef9fa977b255abf2c3e32d
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-05 12:38:02 +0900

    p312 タグ付きユニオンの練習

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-import { answer1 } from "./06_Type/08_力試し";
+import { answer2, Option } from "./06_Type/08_力試し";
 
-answer1();
+const four: Option<number> = {
+  tag: "some",
+  value: 1000,
+};
+
+answer2(four);
+answer2({ tag: "none" });
