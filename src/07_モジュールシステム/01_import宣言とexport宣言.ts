dd6a67ddb7c3f88ac60e0ee472e95ccb4b718a85
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-07 07:19:30 +0900

    p328 型のインポート

--- src/07_モジュールシステム/01_import宣言とexport宣言.ts
+++ src/07_モジュールシステム/01_import宣言とexport宣言.ts
@@ -1,8 +1,18 @@
 const name = "uhyo";
 const age = 26;
 
-export { name as uhyoName, age };
+export { name as uhyoName, age, Animal, tame };
 
 export const getUhyoName = () => {
   return name;
+};
+
+type Animal = {
+  species: string;
+  age: number;
+};
+
+const tame: Animal = {
+  species: "cat",
+  age: 2,
 };
