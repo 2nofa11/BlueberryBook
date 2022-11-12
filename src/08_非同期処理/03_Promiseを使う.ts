0f01e1556c9499f4704d63daaa734c72d810f578
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-12 14:40:36 +0900

    p362 promise.allによる一括実行

--- src/08_非同期処理/03_Promiseを使う.ts
+++ src/08_非同期処理/03_Promiseを使う.ts
@@ -78,3 +78,22 @@ export const staticPromise = () => {
   });
   console.log("1");
 };
+
+export const promiseAll = () => {
+  const pFoo = readFile("foo.txt", "utf8");
+  const pBar = readFile("foo.txt", "utf8");
+  const pBaz = readFile("baz.txt", "utf8");
+
+  const p = Promise.all([pFoo, pBar, pBaz]);
+
+  p.then(
+    (result) => {
+      console.log("foo.txt", result[0]);
+      console.log("bar.txt", result[1]);
+      console.log("baz.txt", result[2]);
+    },
+    (err) => {
+      console.log("失敗");
+    }
+  );
+};
