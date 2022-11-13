0446cb96596a2c5ce2557c85712e80035b92f5e4
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-13 11:03:48 +0900

    p366 コミット漏れ

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-import {
-  promiseAll,
-  allSelectedPromise,
-} from "./08_非同期処理/03_Promiseを使う";
-promiseAll();
-allSelectedPromise();
+import { anyProcess } from "./08_非同期処理/03_Promiseを使う";
+anyProcess();
