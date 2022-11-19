2d2b24f277dd82f3263a797cda8088a81f1b165a
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-19 13:17:35 +0900

    p387 raceを使って可読性を向上させる

--- src/08_非同期処理/05_力試し.ts
+++ src/08_非同期処理/05_力試し.ts
@@ -5,17 +5,21 @@ export const calcUhyo = async (): Promise<number> => {
   let currentIndex = 0;
   const sleep = (duration: number) => {
     return new Promise<number>((resolve, reject) => {
-      setTimeout(reject, duration);
+      setTimeout(resolve, duration);
     });
   };
-
+  const errorAwait = async () => {
+    await sleep(1);
+    throw new Error("TO!");
+  };
   console.log("非同期スタート");
-  sleep(1).catch((e) => {
-    console.log("時間切れ");
-    count = 0;
-    return 0;
-  });
-  const data = await readFile("uhyo.txt", "utf8").then();
+
+  const data = await Promise.race([
+    readFile("uhyo.txt", "utf8"),
+    errorAwait(),
+  ]).catch(() => {
+    return "";
+  }); //失敗した場合はこっちに突っ込まれる
 
   while (true) {
     const nextIndex = data.indexOf("uhyo", currentIndex);
