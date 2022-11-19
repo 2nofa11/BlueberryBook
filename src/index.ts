dec70eda00cf1bb7c07e7a7c0582a608290fabd9
2nofa11 <aboubthethirdkimi@icloud.com>
2022-11-19 10:09:28 +0900

    p383 非同期処理のアロー関数式例

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { awaitFunc4 } from "./08_非同期処理/04_async・await構文";
-awaitFunc4().then(() => {
-  console.log("mainが完了しました。");
+import { allowAsync } from "./08_非同期処理/04_async・await構文";
+allowAsync().then(() => {
+  console.log("allowAsyncが完了しました。");
 });
