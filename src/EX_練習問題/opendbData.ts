a2f86a8720464f3dda384becf3f3c4af03f0b8f5
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-25 11:17:24 +0900

    feat: 型を抽出

--- src/EX_練習問題/opendbData.ts
+++ src/EX_練習問題/opendbData.ts
@@ -1,212 +1,149 @@
-export type OpenDBResponse = typeof openDBType;
-const openDBType = {
+export type openDBType = {
   onix: {
-    RecordReference: "9784101152097",
-    NotificationType: "03",
+    RecordReference: string;
+    NotificationType: string;
     ProductIdentifier: {
-      ProductIDType: "15",
-      IDValue: "9784101152097",
-    },
+      ProductIDType: string;
+      IDValue: string;
+    };
     DescriptiveDetail: {
-      ProductComposition: "00",
-      ProductForm: "BA",
-      ProductFormDetail: "B111",
-      Measure: [
-        {
-          MeasureType: "01",
-          Measurement: "151",
-          MeasureUnitCode: "mm",
-        },
-        {
-          MeasureType: "02",
-          Measurement: "106",
-          MeasureUnitCode: "mm",
-        },
-        { MeasureType: "03", Measurement: "16", MeasureUnitCode: "mm" },
-        { MeasureType: "08", Measurement: "243", MeasureUnitCode: "gr" },
-      ],
+      ProductComposition: string;
+      ProductForm: string;
+      ProductFormDetail: string;
+      Measure: {
+        MeasureType: string;
+        Measurement: string;
+        MeasureUnitCode: string;
+      }[];
       Collection: {
-        CollectionType: "10",
+        CollectionType: string;
         TitleDetail: {
-          TitleType: "01",
-          TitleElement: [
-            {
-              TitleElementLevel: "02",
-              TitleText: {
-                collationkey: "シンチョウブンコ",
-                content: "新潮文庫　し-9-9",
-              },
-            },
-            {
-              TitleElementLevel: "03",
-              TitleText: {
-                collationkey: "シンチョウブンコ",
-                content: "新潮文庫",
-              },
-            },
-          ],
-        },
-      },
+          TitleType: string;
+          TitleElement: {
+            TitleElementLevel: string;
+            TitleText: {
+              collationkey: string;
+              content: string;
+            };
+          }[];
+        };
+      };
       TitleDetail: {
-        TitleType: "01",
+        TitleType: string;
         TitleElement: {
-          TitleElementLevel: "01",
+          TitleElementLevel: string;
           TitleText: {
-            collationkey: "モエヨケン2",
-            content: "燃えよ剣 下",
-          },
-        },
-      },
-      Contributor: [
-        {
-          SequenceNumber: "1",
-          ContributorRole: ["A01"],
-          PersonName: {
-            collationkey: "シバ リョウタロウ",
-            content: "司馬 遼太郎",
-          },
-        },
-      ],
-      Language: [{ LanguageRole: "01", LanguageCode: "jpn" }],
-      Extent: [{ ExtentType: "11", ExtentValue: "560", ExtentUnit: "03" }],
-      Subject: [
-        {
-          MainSubject: "",
-          SubjectSchemeIdentifier: "78",
-          SubjectCode: "0193",
-        },
-        { SubjectSchemeIdentifier: "79", SubjectCode: "31" },
-      ],
-      Audience: [{ AudienceCodeType: "22", AudienceCodeValue: "00" }],
-    },
+            collationkey: string;
+            content: string;
+          };
+        };
+      };
+      Contributor: {
+        SequenceNumber: string;
+        ContributorRole: string[];
+        PersonName: {
+          collationkey: string;
+          content: string;
+        };
+      }[];
+      Language: {
+        LanguageRole: string;
+        LanguageCode: string;
+      }[];
+      Extent: {
+        ExtentType: string;
+        ExtentValue: string;
+        ExtentUnit: string;
+      }[];
+      Subject: (
+        | {
+            MainSubject: string;
+            SubjectSchemeIdentifier: string;
+            SubjectCode: string;
+          }
+        | {
+            SubjectSchemeIdentifier: string;
+            SubjectCode: string;
+            MainSubject?: undefined;
+          }
+      )[];
+      Audience: {
+        AudienceCodeType: string;
+        AudienceCodeValue: string;
+      }[];
+    };
     CollateralDetail: {
-      TextContent: [
-        {
-          TextType: "04",
-          ContentAudience: "00",
-          Text: "二条中洲の決闘\n菊章旗\nお雪と\n江戸日記\n剣の 運命\n大暗転\n伏見の歳三\n鳥羽伏見の戦い・その一\n鳥羽伏見の戦い・その二\n鳥羽伏見の戦い・その 三\n鳥羽伏見の戦い・その四\n大坂の歳三\n松林\n西昭庵\n江戸へ\n北征\n甲州進撃\n勝沼の戦い\n流山 屯集\n袂別\n大鳥圭介\n城攻め\n沖田総司\n陸軍奉行並\n艦隊北上\n小姓市村鉄之助\n松前城略取\n甲鉄 艦\n宮古湾海戦\n襲撃\n再会\n官軍上陸\n五稜郭\n砲煙\n解説　陳 舜臣",
-        },
-      ],
-      SupportingResource: [
-        {
-          ResourceContentType: "01",
-          ContentAudience: "01",
-          ResourceMode: "03",
-          ResourceVersion: [
-            {
-              ResourceForm: "02",
-              ResourceVersionFeature: [
-                {
-                  ResourceVersionFeatureType: "01",
-                  FeatureValue: "D502",
-                },
-                {
-                  ResourceVersionFeatureType: "04",
-                  FeatureValue: "9784101152097.jpg",
-                },
-              ],
-              ResourceLink: "https://cover.openbd.jp/9784101152097.jpg",
-            },
-          ],
-        },
-      ],
-    },
+      TextContent: {
+        TextType: string;
+        ContentAudience: string;
+        Text: string;
+      }[];
+      SupportingResource: {
+        ResourceContentType: string;
+        ContentAudience: string;
+        ResourceMode: string;
+        ResourceVersion: {
+          ResourceForm: string;
+          ResourceVersionFeature: {
+            ResourceVersionFeatureType: string;
+            FeatureValue: string;
+          }[];
+          ResourceLink: string;
+        }[];
+      }[];
+    };
     PublishingDetail: {
       Imprint: {
-        ImprintIdentifier: [
-          { ImprintIDType: "19", IDValue: "10" },
-          { ImprintIDType: "24", IDValue: "3162" },
-        ],
-        ImprintName: "新潮社",
-      },
-      PublishingDate: [
-        { PublishingDateRole: "01", Date: "19720619" },
-        { PublishingDateRole: "11", Date: "19720615" },
-      ],
-    },
+        ImprintIdentifier: {
+          ImprintIDType: string;
+          IDValue: string;
+        }[];
+        ImprintName: string;
+      };
+      PublishingDate: {
+        PublishingDateRole: string;
+        Date: string;
+      }[];
+    };
     ProductSupply: {
-      MarketPublishingDetail: { MarketPublishingStatus: "00" },
+      MarketPublishingDetail: {
+        MarketPublishingStatus: string;
+      };
       SupplyDetail: {
-        ProductAvailability: "99",
-        Price: [{ PriceType: "03", PriceAmount: "850", CurrencyCode: "JPY" }],
-      },
-    },
-  },
+        ProductAvailability: string;
+        Price: {
+          PriceType: string;
+          PriceAmount: string;
+          CurrencyCode: string;
+        }[];
+      };
+    };
+  };
   hanmoto: {
-    datecreated: "2015-08-20 05:07:34",
-    reviews: [
-      {
-        post_user: "genkina",
-        reviewer: "斎藤孝（明治大学教授）",
-        source_id: 20,
-        kubun_id: 1,
-        source: "読売新聞",
-        choyukan: "",
-        han: "",
-        link: "",
-        appearance: "2012-04-15",
-        gou: "",
-      },
-      {
-        post_user: "genkina",
-        reviewer: "高橋千劔破（作家、評論家）",
-        source_id: 19,
-        kubun_id: 1,
-        source: "東京新聞／中日新聞",
-        choyukan: "",
-        han: "",
-        link: "",
-        appearance: "2015-01-25",
-        gou: "",
-      },
-      {
-        post_user: "genkina",
-        reviewer: "末國善己（文芸評論家）",
-        source_id: 18,
-        kubun_id: 1,
-        source: "朝日新聞",
-        choyukan: "朝 刊",
-        han: "",
-        link: "",
-        appearance: "2018-03-04",
-        gou: "",
-      },
-      {
-        post_user: "genkina",
-        reviewer: " 長栄周作（パナソニック会長）",
-        source_id: 23,
-        kubun_id: 1,
-        source: "日本経済新聞",
-        choyukan: " 朝刊",
-        han: "",
-        link: "",
-        appearance: "2018-03-17",
-        gou: "",
-      },
-      {
-        post_user: "genkina",
-        reviewer: "",
-        source_id: 20,
-        kubun_id: 1,
-        source: "読売新聞",
-        choyukan: "朝刊",
-        han: "",
-        link: "",
-        appearance: "2020-10-25",
-        gou: "",
-      },
-    ],
-    dateshuppan: "1972-01",
-    datemodified: "2015-08-20 05:07:34",
-  },
+    datecreated: string;
+    reviews: {
+      post_user: string;
+      reviewer: string;
+      source_id: number;
+      kubun_id: number;
+      source: string;
+      choyukan: string;
+      han: string;
+      link: string;
+      appearance: string;
+      gou: string;
+    }[];
+    dateshuppan: string;
+    datemodified: string;
+  };
   summary: {
-    isbn: "9784101152097",
-    title: "燃えよ剣 下",
-    volume: "",
-    series: "新潮文庫",
-    publisher: "新潮社",
-    pubdate: "19720619",
-    cover: "https://cover.openbd.jp/9784101152097.jpg",
-    author: " 司馬遼太郎／著",
-  },
+    isbn: string;
+    title: string;
+    volume: string;
+    series: string;
+    publisher: string;
+    pubdate: string;
+    cover: string;
+    author: string;
+  };
 };
