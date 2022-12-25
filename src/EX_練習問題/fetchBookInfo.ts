c9401be110064bac3a0e1b0fb7f9d7a69567b015
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-25 11:17:36 +0900

    コードを整理する

--- src/EX_練習問題/fetchBookInfo.ts
+++ src/EX_練習問題/fetchBookInfo.ts
@@ -1,17 +1,13 @@
 import axios, { AxiosResponse } from "axios";
-import { OpenDBResponse } from "./opendbData";
+import { openDBType } from "./opendbData";
 
 export const fetchBookInfo = () => {
   // APIを取得
-  const query = "978-4-8399-7106-9";
+  const query = "978-4-7853-0001-2";
   const url = `https://api.openbd.jp/v1/get?isbn=${query}&pretty`;
-
   const bookInfo = axios(url);
-  bookInfo.then((response: AxiosResponse<OpenDBResponse[]>) => {
-    const { data, status } = response;
-    console.log(
-      JSON.stringify(data[0].onix.DescriptiveDetail.TitleDetail.TitleElement)
-    );
+  bookInfo.then((response: AxiosResponse<openDBType[]>) => {
+    const { data } = response;
     console.log(JSON.stringify(data[0].onix.CollateralDetail.TextContent));
   });
 };
